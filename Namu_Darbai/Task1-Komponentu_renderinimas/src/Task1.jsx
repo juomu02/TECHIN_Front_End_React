e54f644400836d108bc75243ba62402080954cbc
juozas <muleviciusj@gmail.com>
2026-02-19 20:55:28 +0200

    Task1 komponentu_renderinimas

--- Namu_Darbai/Task1-Komponentu_renderinimas/src/Task1.jsx
+++ Namu_Darbai/Task1-Komponentu_renderinimas/src/Task1.jsx
@@ -3,53 +3,20 @@ import profileImage from "./assets/profile.jpg";
 function Task1() {
   return (
     <>
-      <div className="w-screen p-6 bg-green-600">
-        <div className="p-30 pt-5 pb-5 bg-blue-400">
-          <h1 className="text-5xl">Welcome to 30 Days Of React</h1>
-          <h2 className="text-3x1">Getting Started React</h2>
-          <h3 className="text-2x1">JavaScript Library</h3>
-          <h4 className="text-sm">Instructor: Asabeneh Yetayeh</h4>
-          <p className="text-sm">Date: Oct 3, 2020</p>
+      <div className="w-screen p-6 bg-white font-mono">
+        <div className="p-5 pt-5 pb-5 bg-blue-100  rounded-3xl text-center">
+          <h1 className="text-3xl pb-10">SUBSCRIBE</h1>
+          <p className="pb-10">Sign up with your email address to receive news and updates.</p>
+
+          <form className="flex-col" action="submit">
+            <div className="flex pt-5 pb-5 gap-10 justify-center">
+            <input className="bg-white rounded-2xl p-3" type="text" placeholder="First name" />
+            <input className="bg-white rounded-2xl p-3" type="text" placeholder="Last name" />
+            <input className="bg-white rounded-2xl p-3" type="email" placeholder="Email" /></div>
+            <input className="bg-red-300 pb-3 pt-3 w-80 rounded-2xl text-white font-bold" type="submit" value="Submit" />
+          </form>
         </div>
       </div>
-
-      <div className="w-screen p-6 bg-amber-200">
-        <div className="pt-5 pb-5 bg-white">
-          <div className="p-30 pt-5 pb-5">
-            <div className="p-6 bg-purple-900">
-        <div className="pt-5 pb-5 bg-white">
-              <p>
-                Prerequisite to get started
-                <span className="bold">react.js</span>
-              </p>
-              <ul className="pl-5">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-              </ul>
-              <p>3+2=5</p>
-              <p>
-                I am Asabeneh Yetayeh. I am 200 years old. I love React and
-                thank you for joining 30 Days Of React challenge.
-              </p>
-            </div>
-            </div>
-            <div className="p-6 bg-blue-600">
-        <div className="bg-white">
-              <img className="rounded-full" src={profileImage} alt="profile pic" />
-              <p>Asabeneh Yetayeh</p>
-            </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-      <div className="w-screen p-6 bg-red-500">
-        <div className="p-30 pt-5 pb-5 bg-blue-300">
-          <p className="text-center">Copyright &#169; 2020</p>
-            </div>
-          </div>
     </>
   );
 }
