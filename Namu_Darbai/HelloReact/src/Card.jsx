4d04fd11693cc1cefdf272aa5e28923054138778
juozas <muleviciusj@gmail.com>
2026-02-19 19:35:39 +0200

    .

--- Namu_Darbai/HelloReact/src/Card.jsx
+++ Namu_Darbai/HelloReact/src/Card.jsx
@@ -1,9 +1,9 @@
-import productImage from "./assets/spirgis.jpg"
+import productImage from "./assets/spirgis.jpg";
 
 function Card() {
   return (
-    <div>
-        <img src={productImage} alt="spirgis" />
+    <div className="p-6 bg-slate-200 flex justify-center">
+      <img src={productImage} alt="spirgis" />
       <h1>My card</h1>
       <p>Lorem ipsum</p>
     </div>
