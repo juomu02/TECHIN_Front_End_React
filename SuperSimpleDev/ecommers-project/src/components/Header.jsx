8588d2dc7fca9a5bc1bebd0f26cc9fe7f3cb80c4
juozas <muleviciusj@gmail.com>
2026-03-03 16:25:00 +0200

    Changed to NavLink in header links

--- SuperSimpleDev/ecommers-project/src/components/Header.jsx
+++ SuperSimpleDev/ecommers-project/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import "./Header.css";
 
 function Header() {
@@ -6,10 +6,10 @@ function Header() {
     <>
       <div className="header">
         <div className="left-section">
-          <Link to="/" className="header-link">
+          <NavLink to="/" className="header-link">
             <img className="logo" src="images/logo-white.png" />
             <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </Link>
+          </NavLink>
         </div>
         <div className="middle-section">
           <input className="search-bar" type="text" placeholder="Search" />
@@ -20,15 +20,15 @@ function Header() {
         </div>
 
         <div className="right-section">
-          <Link className="orders-link header-link" to="/orders">
+          <NavLink className="orders-link header-link" to="/orders">
             <span className="orders-text">Orders</span>
-          </Link>
+          </NavLink>
 
-          <Link className="cart-link header-link" to="/checkout">
+          <NavLink className="cart-link header-link" to="/checkout">
             <img className="cart-icon" src="images/icons/cart-icon.png" />
             <div className="cart-quantity">3</div>
             <div className="cart-text">Cart</div>
-          </Link>
+          </NavLink>
         </div>
       </div>
     </>
